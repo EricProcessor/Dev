9d2ddb54a65ca6ea893fce37dcffe9f8c3db775e
xinShi <1014692922@qq.com>
2018-12-27 17:25:55 +0800

    shixin

--- ExpressApp1(1)/ExpressApp1/StorageConfig.ts
+++ ExpressApp1(1)/ExpressApp1/StorageConfig.ts
@@ -6,15 +6,16 @@ import {Config} from './Config'
 import { TableSetting } from './AzureTable';
 
 export class StorageConfig {
-
+    
     //@AnCommented public static readonly emulatorConnectionString = 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=base 64 encoded key;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;';
+    //连接Table表
     public static readonly emulatorConnectionString = 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;';
     public static readonly defaultAccountName = "meeservicesstorage";
     public static readonly defaultAccessKey = "base64 encoded key";
 
     public static readonly devAccountName = "meedevelopment";
     public static readonly devAccessKey = "base 64 encoded key";
-
+    //新用户表设置存储位置
     public static getNewUserTableSettings() {
         let tablename = 'stagingmeeusers';
         if (Environment.getEnvironmentType() == EnvironmentType.Production) {
@@ -23,20 +24,21 @@ export class StorageConfig {
 
         return this.getUserTableSettings(tablename, 'oid');
     }
-
+    //用户表设置存储位置
     public static getUserTableSettings(overrideTableName: string = undefined, overrideRowKeyName :string = undefined) : TableSetting{
         let setting : TableSetting = {} as any;
         let environmentType = Environment.getEnvironmentType();
         console.log('getUserTableSettings: EnvironemtType:' + environmentType)   
         setting.partitionKeyName = 'tenantId';
         setting.rowKeyName = overrideRowKeyName || 'unique_name';
-
+        
         if (Config.useLocalEmulator) {
+            /*本地环境的情况下设置*/
             setting.connectionString = StorageConfig.emulatorConnectionString;
             setting.tableName = overrideTableName || 'developmentusers'
         }
-        else{
-    
+        else{//存储位置
+            /*开发环境的情况下设置*/
             switch(environmentType) {
                 case EnvironmentType.Production:
                     setting.accountName = process.env.AZURE_STORAGE_ACCOUNT || StorageConfig.defaultAccountName;
@@ -58,7 +60,7 @@ export class StorageConfig {
     
         return setting;
     }
-
+    //用户数据表设置存储位置
     public static getTelemetryTableSettings() : TableSetting{
         let setting : TableSetting = {} as any;
         let environmentType = Environment.getEnvironmentType();
@@ -91,7 +93,7 @@ export class StorageConfig {
     
         return setting;
     }
-
+    //付费记录表设置？
     public static getReceiptsTableSettings() : TableSetting{
         let setting : TableSetting = {} as any;
         let environmentType = Environment.getEnvironmentType();
