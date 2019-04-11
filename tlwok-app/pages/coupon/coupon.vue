96333d291d1b00e5fd7034cdeaf9423c7ede58cb
1014692922@qq.com <shixin9805>
2019-04-11 18:22:38 +0800

    1

--- tlwok-app/pages/coupon/coupon.vue
+++ tlwok-app/pages/coupon/coupon.vue
@@ -1,14 +1,19 @@
 <template>
 	<view class="uni-tab-bar">
 		<scroll-view id="tab-bar" class="uni-swiper-tab" scroll-x :scroll-left="scrollLeft">
-			<view v-for="(tabItem,index) in tabBars" :key="tabItem.id" class="swiper-tab-list tabItem" :class="tabIndex==index ? 'active' : ''" :id="tabItem.id"
-			 :data-current="index" @click="tapTab">{{tabItem.name}}( {{tabItem.number}} )</view>
+			<view v-for="(tabItem,index) in tabBars" 
+			 :key="index" 
+			 class="swiper-tab-list tabItem" 
+			 :class="tabIndex==index ? 'active' : ''" :id="tabItem.id"
+			 :data-current="index" @click="tapTab(index)">
+				{{tabItem.name}}({{tabItem.number}})
+			</view>
 		</scroll-view>
 		<swiper :current="tabIndex" class="swiper-box" :duration="300" @change="changeTab">
 			<swiper-item v-for="(coupons,index1) in couponItems" :key="index1">
 				<scroll-view class="list" scroll-y @scrolltolower="loadMore(index1)">
 					<block v-for="(item,index2) in coupons" :key="index2">
-						<couponList :options="item" @close="close(index1,index2)"></couponList>
+						<couponList :options="item" :index="index1"></couponList>
 					</block>
 					<!-- <view class="uni-tab-bar-loading">
 						{{tab.loadingText}}
@@ -28,6 +33,9 @@
 		},
 		data() {
 			return {
+				scrollLeft: 0,
+				tabIndex: 0,
+				isClickChange: false,
 				tabBars: [{
 					name: '未使用',
 					status: '0',
@@ -45,55 +53,52 @@
 					number: 10,
 					id: 'expired'
 				}],
-				scrollLeft: 0,
-				tabIndex: 0,
-				isClickChange: false,
 				couponItems: {
+					noused:[{
+						name:'12.21快乐共和长尾夹体验券(5)',
+						applicable_scope:'乐活文具(未使用)',
+						price:50,
+						priceRange:500,
+						timeRange:'2019.03.30-2019.04.30'
+					},{
+						name:'12.21快乐共和长尾夹体验券(6)',
+						applicable_scope:'乐活文具(未使用)',
+						price:50,
+						priceRange:500,
+						timeRange:'2019.03.30-2019.04.30'
+					}],
+					used:[{
+						name:'12.21快乐共和长尾夹体验券(3)',
+						applicable_scope:'乐活文具(已使用)',
+						price:50,
+						priceRange:500,
+						timeRange:'2019.03.30-2019.04.30'
+					},{
+						name:'12.21快乐共和长尾夹体验券(4)',
+						applicable_scope:'乐活文具(已使用)',
+						price:50,
+						priceRange:500,
+						timeRange:'2019.03.30-2019.04.30'
+					}],
 					expired:[{
 						name:'12.21快乐共和长尾夹体验券(1)',
-						applicable_scope:'乐活文具',
+						applicable_scope:'乐活文具(已过期)',
 						price:50,
 						priceRange:500,
 						timeRange:'2019.03.30-2019.04.10'
 					},
 					{
 						name:'12.21快乐共和长尾夹体验券(1)',
-						applicable_scope:'乐活文具',
+						applicable_scope:'乐活文具(已过期)',
 						price:50,
 						priceRange:500,
 						timeRange:'2019.03.30-2019.04.10'
 					},{
 						name:'12.21快乐共和长尾夹体验券(2)',
-						applicable_scope:'乐活文具',
+						applicable_scope:'乐活文具(已过期)',
 						price:50,
 						priceRange:500,
 						timeRange:'2019.03.30-2019.04.10'
-					}],
-					used:[{
-						name:'12.21快乐共和长尾夹体验券(3)',
-						applicable_scope:'乐活文具',
-						price:50,
-						priceRange:500,
-						timeRange:'2019.03.30-2019.04.30'
-					},{
-						name:'12.21快乐共和长尾夹体验券(4)',
-						applicable_scope:'乐活文具',
-						price:50,
-						priceRange:500,
-						timeRange:'2019.03.30-2019.04.30'
-					}],
-					noused:[{
-						name:'12.21快乐共和长尾夹体验券(5)',
-						applicable_scope:'乐活文具',
-						price:50,
-						priceRange:500,
-						timeRange:'2019.03.30-2019.04.30'
-					},{
-						name:'12.21快乐共和长尾夹体验券(6)',
-						applicable_scope:'乐活文具',
-						price:50,
-						priceRange:500,
-						timeRange:'2019.03.30-2019.04.30'
 					}]
 				}
 			}
@@ -102,16 +107,6 @@
 			
 		},
 		methods: {
-			close(index1, index2) {
-				uni.showModal({
-					content: '是否删除本条信息？',
-					success: (res) => {
-						if (res.confirm) {
-							this.newsitems[index1].data.splice(index2, 1);
-						}
-					}
-				})
-			},
 			loadMore(e) {
 				this.newsitems[e].loadingType = 1;
 				setTimeout(() => {
@@ -174,10 +169,14 @@
 	.uni-tab-bar{
 		.uni-swiper-tab{ //couponHeader
 			background: #ffffff;
+			border: none;
 			.tabItem {
 				display: inline-block;
 				width: 33%!important;
 				text-align: center;
+				height: 50px;
+				line-height: 50px;
+				box-sizing: border-box;
 			}
 			.active{
 				color: #e4393c;
