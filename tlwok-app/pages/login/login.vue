600343af31c69fa01f959ad3bc82f90dee68046c
Eric <17600100962@163.com>
2019-04-26 18:30:07 +0800

    llh

--- tlwok-app/pages/login/login.vue
+++ tlwok-app/pages/login/login.vue
@@ -2,11 +2,11 @@
 	<view class="Content">
 		<view class="loginForm">
 			<view class="loginItem">
-				<input type="text" value="" placeholder="用户名/手机号"/>
+				<input type="text" value="" placeholder="用户名/手机号" />
 				<i class="tlwok-icon">&#xe736</i>
 			</view>
 			<view class="loginItem">
-				<input type="text" value="" placeholder="请输入密码"/>
+				<input type="text" value="" placeholder="请输入密码" />
 				<i class="tlwok-icon">&#xe6c0</i>
 			</view>
 			<view class="loginItem forgetPwd">
@@ -16,11 +16,11 @@
 				<button type="primary">登录</button>
 			</view>
 		</view>
-		<view class="otherLogin">
+		<view class="otherLogin" v-if="aouthWx">
 			<span>其他方式登录</span>
 		</view>
-		<view class="wxLogin">
-			<i class="tlwok-icon" >&#xe73f</i>
+		<view class="wxLogin" @tap="loginWx" v-if="aouthWx">
+			<i class="tlwok-icon">&#xe73f</i>
 		</view>
 		<view class="toRegister">
 			还没有账号？<span @tap="toRegister">立即注册→</span>
@@ -32,18 +32,62 @@
 	export default {
 		data() {
 			return {
-				
+				aouthWx: false
 			};
 		},
-		methods:{
-			toRegister:function (){
+		onLoad: function() {
+			// #ifdef APP-PLUS
+			this.aouthWx = true;
+			// #endif
+		},
+		methods: {
+			toRegister: function() {
 				uni.navigateTo({
 					url: '/pages/register/register',
 					animationType: 'pop-in',
 					animationDuration: 200
 				});
 			},
-			togetPass:function (){
+			loginWx: function() {
+				if (plus.runtime.isApplicationExist({
+						pname: 'com.tencent.mm',
+						action: 'weixin://'
+					})) {
+					console.log("微信应用已安装");
+				} else {
+					console.log("微信应用未安装");
+				};
+				uni.getProvider({
+					service: 'oauth',
+					success: function(res) {
+						console.log(res.provider);
+
+						//支持微信、qq和微博等
+						if (~res.provider.indexOf('weixin')) {
+							uni.login({
+								provider: 'weixin',
+								scopes: 'auth_user',
+								success: function(loginRes) {
+									console.log('-------获取openid(unionid)-----');
+									console.log(JSON.stringify(loginRes));
+									// 获取用户信息
+									uni.getUserInfo({
+										provider: 'weixin',
+										success: function(infoRes) {
+											console.log('-------获取微信用户所有-----');
+											console.log(JSON.stringify(infoRes));
+										}
+									});
+								},
+								fail: function(err) {
+									console.log(JSON.stringify(err));
+								}
+							});
+						}
+					}
+				});
+			},
+			togetPass: function() {
 				uni.navigateTo({
 					url: '/pages/getpass/getpass',
 					animationType: 'pop-in',
